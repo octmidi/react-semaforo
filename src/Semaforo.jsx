04f4729d934dd2cf0d7f906466565e717711838b
Daniel G. <octmidi@hotmail.com>
2023-11-11 18:28:26 -0300

    new ligth

--- src/Semaforo.jsx
+++ src/Semaforo.jsx
@@ -7,26 +7,44 @@ const Semaforo = () => {
     const [redLight, setRedLight] = useState(false);
     const [yellowLight, setYellowLight] = useState(false);
     const [greenLight, setGreenLight] = useState(false);
+    const [blueLight, setBlueLight] = useState(false);
+    const [newLigth, setNewLigth] = useState(false);
+    const [divDisplay, setDivDisplay] = useState(false);
+
 
 
     const handleRedClick = () => {
         setRedLight(true);
         setYellowLight(false);
         setGreenLight(false);
+        setBlueLight(false);
     };
 
     const handleYellowClick = () => {
         setRedLight(false);
         setYellowLight(true);
         setGreenLight(false);
+        setBlueLight(false);
     };
 
     const handleGreenClick = () => {
         setRedLight(false);
         setYellowLight(false);
         setGreenLight(true);
+        setBlueLight(false);
     };
 
+    const handlePurpleClick = () => {
+        setRedLight(false);
+        setYellowLight(false);
+        setGreenLight(false);
+        setBlueLight(true);
+    };
+    ;
+
+    const handleMostrar = () => {
+        setDivDisplay(!divDisplay);
+    };
     return (
         <>
             <div className="traffic-light-container">
@@ -42,8 +60,17 @@ const Semaforo = () => {
                     onClick={handleGreenClick}
                     className={`light ${greenLight ? 'green' : 'greenoff'}`}
                 />
+                <div
+                    id='newligth'
+                    onClick={handlePurpleClick}
+                    className={`light ${blueLight ? 'blueLigth' : 'blueLigtoff'}`}
+                    style={{ display: divDisplay ? 'block' : 'none' }}
+                />
             </div>
-            <div className='pilar'></div>
+            <div className='pilar'>
+                <button className='buttonligth' onClick={handleMostrar}>Luz</button>
+            </div>
+
         </>
     );
 };
